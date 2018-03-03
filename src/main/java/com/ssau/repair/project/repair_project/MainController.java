07caccff78bf22d1eb622bd02cfe955950a0c107
Dmi3 <m-dimon93@yandex.ru>
2018-03-03 15:17:07 +0400

    Initial develop

---
+++ src/main/java/com/ssau/repair/project/repair_project/MainController.java
@@ -0,0 +1,20 @@
+package com.ssau.repair.project.repair_project;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class MainController
+{
+    private static final String message = "hello world";
+
+    @RequestMapping("/")
+    public  String welcome(Map<String,Object> model)
+    {
+        model.put("message", message);
+        return "welcome";
+    }
+}
---
+++ src/main/java/com/ssau/repair/project/repair_project/RepairProjectApplication.java
@@ -0,0 +1,12 @@
+package com.ssau.repair.project.repair_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairProjectApplication.class, args);
+	}
+}
